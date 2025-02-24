1c1adb072ddc27cfe49b7484b6446b879f85805c
Mason Jennings <masonj@csu.fullerton.edu>
2025-02-24 03:16:13 -0800

    memoizes values 0-91

--- factorial.c
+++ factorial.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-typedef long (*long_func_ptr)(int of_num);
+typedef long long (*long_func_ptr)(int of_num);
 long_func_ptr original_provider;
 long_func_ptr factorial_provider;
 
 #define NOT_PRESENT -1
-long results[100] = {NOT_PRESENT};
+long long results[100] = {NOT_PRESENT};
 
-long cache(int val) {
+long long cache(int val) {
     for (int ix = 0; ix < 100; ix++)
         results[ix] = NOT_PRESENT;
 
@@ -17,22 +17,31 @@ long cache(int val) {
 }
 
 // Provider
-long factorial(int of_num) {
+long long factorial(int of_num) {
     if (of_num < 2) {
-        // printf("hello from factorial!\n");
         return 1;
     }
-    return of_num * (*factorial_provider)(of_num - 1);
+    return (long long)of_num * (*factorial_provider)(of_num - 1);
 }
 
 int main() {
-    // printf("HELLO C\n");
-    int n              = 9;
+    for (int ix = 0; ix < 92; ix++) {
+        int n              = ix;
 
-    original_provider  = factorial;
-    factorial_provider = cache;
+        original_provider  = factorial;
+        factorial_provider = cache;
 
-    printf("factorial of %d is %ld\n", n, (*factorial_provider)(n));
+        printf("factorial of %d is %lld\n", n, (*factorial_provider)(n));
+    }
+
+    for (int ix = 0; ix < 92; ix++) {
+        int n              = ix;
+
+        original_provider  = factorial;
+        factorial_provider = cache;
+
+        printf("factorial of %d is %lld\n", n, (*factorial_provider)(n));
+    }
 
     return 0;
 }
