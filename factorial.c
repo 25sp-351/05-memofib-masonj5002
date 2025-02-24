663f5a37eb60e45baad9fee2a6dd063d5a394f3f
Mason Jennings <masonj@csu.fullerton.edu>
2025-02-24 02:47:18 -0800

    ran clang-format

--- factorial.c
+++ factorial.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 typedef long (*long_func_ptr)(int of_num);
-long_func_ptr original_provider = NULL;
+long_func_ptr original_provider  = NULL;
 long_func_ptr factorial_provider = NULL;
 
 #define NOT_PRESENT -1
 long results[100] = {NOT_PRESENT};
 
 long cache(int val) {
-    if(results[val] == NOT_PRESENT) {
+    if (results[val] == NOT_PRESENT)
         results[val] = (*original_provider)(val);
-    }
     return results[val];
 }
 
@@ -20,14 +19,14 @@ long factorial(int of_num) {
         printf("hello from factorial!\n");
         return 1;
     }
-    return of_num * (*factorial_provider)(of_num-1);
+    return of_num * (*factorial_provider)(of_num - 1);
 }
 
 int main() {
     printf("HELLO C\n");
-    int n = 5;
+    int n              = 5;
 
-    original_provider = factorial;
+    original_provider  = factorial;
     factorial_provider = cache;
 
     printf("factorial of %d is %ld\n", n, (*factorial_provider)(n));
